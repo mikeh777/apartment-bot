0af6738a8191c70dc490af612923df17104dc2af
mikehenry <mhenry@alumni.scu.edu>
2025-05-06 17:59:29 -0700

    Updated for new search parameters

--- settings.py
+++ settings.py
@@ -11,9 +11,9 @@ CL_SITE = 'sfbay'
 CL_AREA = 'sfc'
 CL_CATEGORY = 'apa'
 MAX_NUM_BEDROOMS = 3
-MIN_NUM_BEDROOMS = 3
+MIN_NUM_BEDROOMS = 2
 POSTED_TODAY = True
-MAX_PRICE = 6000
+MAX_PRICE = 7000
 MIN_PRICE = 4500
 LAUNDRY_OPTIONS = ['w/d hookups', 'w/d in unit', 'laundry in bldg', 'laundry on site']
 PARKING_OPTIONS = ['carport', 'attached garage', 'detached garage']
@@ -22,23 +22,23 @@ HOUSING_TYPE = ['apartment', 'condo', 'flat', 'house', 'loft']
 ##Housing variables
 #define areas that we want to live in
 AREAS = {
-    "russian-hill": [
-        [-122.424929, 37.795946],
-        [-122.410338, 37.805179]
+    "marina": [
+        [-122.448284,37.792127],
+        [-122.42378, 37.807353]
     ],
-    "nob-hill":
-    [
-        [-122.421748, 37.788999],
-        [-122.408916, 37.799477]
-    ],
-    "mission":
-    [
-        [-122.430904, 37.757974],
-        [-122.423394, 37.764963]
-    ]
+    # "nob-hill":
+    # [
+    #     [-122.421748, 37.788999],
+    #     [-122.408916, 37.799477]
+    # ],
+    # "mission":
+    # [
+    #     [-122.430904, 37.757974],
+    #     [-122.423394, 37.764963]
+    # ]
 }
 #fall back neighborhoods to try and to find in the listing string
-NEIGHBORHOODS = ["russian hill",
-                 "nob hill"]
+NEIGHBORHOODS = ["marina",
+                 "cow holoow"]
 
 SLEEP_TIMER = 20 * 60
